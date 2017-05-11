806a83b6377176b792eed42a8303539c1afba07b
PrimeAC <afonso.caetano@outlook.com>
2017-05-11 16:40:49 +0100

    Change drawBoard to a cycle (#closes 5)

--- client.py
+++ client.py
@@ -40,21 +40,32 @@ def readList(msg):
 #TIC TAC TOE
 def drawBoard(board):
     # This function prints out the board that it was passed.
+    output = ''
+    for i in range(1, 17):
+    	if i%4==0:
+    		output = output + board[i]
+    		print(' ' + output)
+    		if i != 16:
+    			print('---+---+---+---')
+    		output = ''
+    	else:
+    		output = output + board[i] + ' | '
+
 
      # "board" is a list of 10 strings representing the board (ignore index 0)    print('   |   |')
-     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + ' | ' + board[4])
+     #print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + ' | ' + board[4])
      #print('   |   |')
-     print('---+---+---+---')
+     #print('---+---+---+---')
      #print('   |   |')
-     print(' ' + board[5] + ' | ' + board[6] + ' | ' + board[7] + ' | ' + board[8])
+     #print(' ' + board[5] + ' | ' + board[6] + ' | ' + board[7] + ' | ' + board[8])
      #print('   |   |')
-     print('---+---+---+---')
+     #print('---+---+---+---')
      #print('   |   |')
-     print(' ' + board[9] + ' | ' + board[10] + ' | ' + board[11] + ' | ' + board[12])
+     #print(' ' + board[9] + ' | ' + board[10] + ' | ' + board[11] + ' | ' + board[12])
      #print('   |   |')
-     print('---+---+---+---')
+     #print('---+---+---+---')
      #print('   |   |')
-     print(' ' + board[13] + ' | ' + board[14] + ' | ' + board[15] + ' | ' + board[16])
+     #print(' ' + board[13] + ' | ' + board[14] + ' | ' + board[15] + ' | ' + board[16])
      #print('   |   |')
 
 def makeMove(board, letter, move):
